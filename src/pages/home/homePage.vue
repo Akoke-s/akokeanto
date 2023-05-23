ce255a4ab5e00298a37f2d42e45cdc5bac030fae
Anto Akoke Victor <antoakoke@gmail.com>
2023-05-23 16:42:10 +0100

    create past current companies

--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -2,6 +2,7 @@
 import topBanner from '@/components/home/topBanner.vue';
 import aboutSection from '@/components/home/aboutSection.vue';
 import myWorks from '@/components/home/myWorks.vue';
+import companyWorked from '@/components/home/companyWorked.vue';
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import myWorks from '@/components/home/myWorks.vue';
         <topBanner />
         <aboutSection />
         <myWorks />
+        <companyWorked />
     </div>
 </template>
