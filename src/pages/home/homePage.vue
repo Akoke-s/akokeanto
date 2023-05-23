98f199fb84bb6f54e347d89f8930c4c225cc421c
Anto Akoke Victor <antoakoke@gmail.com>
2023-05-23 17:32:53 +0100

    add personal experiences

--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -3,6 +3,7 @@ import topBanner from '@/components/home/topBanner.vue';
 import aboutSection from '@/components/home/aboutSection.vue';
 import myWorks from '@/components/home/myWorks.vue';
 import companyWorked from '@/components/home/companyWorked.vue';
+import myExperiences from '@/components/home/myExperiences.vue'
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import companyWorked from '@/components/home/companyWorked.vue';
         <aboutSection />
         <myWorks />
         <companyWorked />
+        <myExperiences />
     </div>
 </template>
