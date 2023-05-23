fc13278b117de261af87b9aac8c262469ccd0553
Anto Akoke Victor <antoakoke@gmail.com>
2023-05-23 16:17:33 +0100

    create featured works section

--- src/pages/home/homePage.vue
+++ src/pages/home/homePage.vue
@@ -1,11 +1,13 @@
 <script setup>
 import topBanner from '@/components/home/topBanner.vue';
 import aboutSection from '@/components/home/aboutSection.vue';
+import myWorks from '@/components/home/myWorks.vue';
 </script>
 
 <template>
     <div>
         <topBanner />
         <aboutSection />
+        <myWorks />
     </div>
 </template>
