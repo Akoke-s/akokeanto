e4d3704deef09ec7df1fdf57197896af13127f79
Anto Akoke Victor <antoakoke@gmail.com>
2023-05-25 18:27:13 +0100

    make company logos responsive

--- src/components/home/companyWorked.vue
+++ src/components/home/companyWorked.vue
@@ -2,43 +2,72 @@
 </script>
 
 <template>
+    
     <div class="bg-white mb-6">
         <div class="mx-auto max-w-5xl px-4 py-4 sm:px-6 lg:px-8">
-            <p class="mx-auto mt-4 max-w-4xl text-center leading-relaxed text-4xl">
+            <p class="mx-auto mt-4 max-w-4xl text-center leading-relaxed text-lg lg:text-4xl">
                 Companies I've helped reach new heights
             </p>
 
-            <div class="flex flex-1 mx-auto justify-center items-center gap-8 mt-8 mb-8">
-                <div class="grayscale hover:grayscale-0">
-                    <img
-                        alt="Office"
-                        src="https://s3-alpha-sig.figma.com/img/f6b3/ac83/39e903d6be773f47c1012c05d7f79409?Expires=1685923200&Signature=AFlXAh7OZYkNKSFClknQ9iMVxlSGpucxkFWitKcCTY7EsfmaGiXt2Lz0trLXi-YWVhjBF414GBcihoVCWY3QZjf9SgEaFbAJky2gFhY3xWXjDqj-0iwDlGxfue44Gr-zLU2H4y61as8iK7ljkqMXZaeXMmT4Ml-rV0uIq9oKRaPGWivIRBvLCXFflhkf~DUGlFGTq1XpRn5kIXs92aQQNiWe23gp8BrbY4utcSHXhG96zZX9P1s-ac7gwe6WRrgjJQ9e5JeoD2Suv~~zUQQvHCTALcJ0aiqYGrMo7yffq3T0z4YGqKsTaVDs1R03yYfO6UEn8jYoYIFIDcEnyz7vXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                        class="h-16 cursor-pointer object-cover"
+        </div>
+
+        <div>
+            <ul class="media-images media-images--config-dynamic">
+                <li class="media-images__item grayscale hover:grayscale-0">
+                    <img 
+                        class="media-images__image h-16" 
+                        src="https://s3-alpha-sig.figma.com/img/f6b3/ac83/39e903d6be773f47c1012c05d7f79409?Expires=1685923200&Signature=AFlXAh7OZYkNKSFClknQ9iMVxlSGpucxkFWitKcCTY7EsfmaGiXt2Lz0trLXi-YWVhjBF414GBcihoVCWY3QZjf9SgEaFbAJky2gFhY3xWXjDqj-0iwDlGxfue44Gr-zLU2H4y61as8iK7ljkqMXZaeXMmT4Ml-rV0uIq9oKRaPGWivIRBvLCXFflhkf~DUGlFGTq1XpRn5kIXs92aQQNiWe23gp8BrbY4utcSHXhG96zZX9P1s-ac7gwe6WRrgjJQ9e5JeoD2Suv~~zUQQvHCTALcJ0aiqYGrMo7yffq3T0z4YGqKsTaVDs1R03yYfO6UEn8jYoYIFIDcEnyz7vXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" 
+                        alt="ef-network-ltd-logo" 
                     />
-                </div>
-                <div class="grayscale hover:grayscale-0">
-                    <img
-                        alt="Office"
-                        src="https://s3-alpha-sig.figma.com/img/efbd/cce4/d4649475a1ab19b7dc9bf12f7472d590?Expires=1685923200&Signature=XfcF4ziBZN6BeThgdRhRD7aY~HTf159YxEFTxB1DR1joBH~Rrq2188mE3SCwRLrJyS3nexwBJIGj4R3tiUCFwhs5DMlGTRzkEzWwNdfzQXAdcjf7Y7P1diNRy60IEh-s2fdrK2a94AKNAaD1TJqmNKurC2xKiN6R~Y~WS-OLcubsCz57LkMhyZM6EBhChyIwzZ6Zn0Aa7jN77M22SX68HvzH8RgX~p7JhYxHR7Zr~riadkQjVD8FF50-n-weDejdRoJcJJrY4V-f02k4DDf1fV5WlFpxvicOIyW0E~PzgAOSIap-c1UgMVTYrnGXZo84hjCtAn1Xp0NiICx3G9xrPQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                        class="h-16 cursor-pointer object-cover"
+                </li>
+                <li class="media-images__item grayscale hover:grayscale-0">
+                    <img 
+                        class="media-images__image h-16" 
+                        src="https://s3-alpha-sig.figma.com/img/efbd/cce4/d4649475a1ab19b7dc9bf12f7472d590?Expires=1685923200&Signature=XfcF4ziBZN6BeThgdRhRD7aY~HTf159YxEFTxB1DR1joBH~Rrq2188mE3SCwRLrJyS3nexwBJIGj4R3tiUCFwhs5DMlGTRzkEzWwNdfzQXAdcjf7Y7P1diNRy60IEh-s2fdrK2a94AKNAaD1TJqmNKurC2xKiN6R~Y~WS-OLcubsCz57LkMhyZM6EBhChyIwzZ6Zn0Aa7jN77M22SX68HvzH8RgX~p7JhYxHR7Zr~riadkQjVD8FF50-n-weDejdRoJcJJrY4V-f02k4DDf1fV5WlFpxvicOIyW0E~PzgAOSIap-c1UgMVTYrnGXZo84hjCtAn1Xp0NiICx3G9xrPQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" 
+                        alt="ourpass-inc-logo" 
                     />
-                </div>
-                <div class="grayscale hover:grayscale-0">
-                    <img
-                        alt="Office"
-                        src="https://s3-alpha-sig.figma.com/img/155e/27d0/e6a6304285bc694df3fdec5033acff6a?Expires=1685923200&Signature=dIFdNRIvsqITAIlaqv9e4IcBnigH5LirOm4wnmSYCHYpOrEBKCjOSjQJlaWW2kehZHc4sftf7-2Zqos9JurXg4CGmKQfU3kNs19848WVy7FBrZHM1e9cZf6Ru7pAf6PNpZmL0XwsoaS4qNWPU8OD7AeOlzhGElfFkCUzqabUT6HGA7bWLiNEVrC4V6UHaKHezLIqgJDcNT5YFkV1sGM8tlK8-HGEeptss9Y7rIdDZ-cUT0jD-HIzgTy5DR24luSdirYcKtksbKNp1kJ2QFbnqLevOLymHXDx4SgMzvBY7CUZH~u1m9UMWw8ewH565CoNcEcbnvuVSga7EUvN206bOg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                        class="h-16 cursor-pointer object-cover"
+                </li>
+                <li class="media-images__item grayscale hover:grayscale-0">
+                    <img 
+                        class="media-images__image h-16" 
+                        src="https://s3-alpha-sig.figma.com/img/155e/27d0/e6a6304285bc694df3fdec5033acff6a?Expires=1685923200&Signature=dIFdNRIvsqITAIlaqv9e4IcBnigH5LirOm4wnmSYCHYpOrEBKCjOSjQJlaWW2kehZHc4sftf7-2Zqos9JurXg4CGmKQfU3kNs19848WVy7FBrZHM1e9cZf6Ru7pAf6PNpZmL0XwsoaS4qNWPU8OD7AeOlzhGElfFkCUzqabUT6HGA7bWLiNEVrC4V6UHaKHezLIqgJDcNT5YFkV1sGM8tlK8-HGEeptss9Y7rIdDZ-cUT0jD-HIzgTy5DR24luSdirYcKtksbKNp1kJ2QFbnqLevOLymHXDx4SgMzvBY7CUZH~u1m9UMWw8ewH565CoNcEcbnvuVSga7EUvN206bOg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" 
+                        alt="invercross-business-solutions-logo" 
                     />
-                </div>
-                <div class="grayscale hover:grayscale-0">
-                    <img
-                        alt="Office"
+                </li>
+                <li class="media-images__item grayscale hover:grayscale-0">
+                    <img 
+                        class="media-images__image h-16" 
                         src="https://s3-alpha-sig.figma.com/img/057d/da46/500c74b6e88c8913cdc82e017db06c5f?Expires=1685923200&Signature=n0lUJ2lNBabYHNu~VjfFFR67n903J9RF8ILgPlu1my9M~TA9IpAPxDsvcvXP2F-zEBkQMoMj~UTu83f33e6-Cdmr1oyVOlKTizk27bblliGSxkBu3tvc54KpjgaE4RlE~pstYeRznGUnSF15Aq1KV5jAOYcXdLOXXjXH-F9xtke94zNzNcUqHG2BcJqgLzXSDYO6iYb8hxHqyQsYi5uZzRLe6gRk~YDUyl2f24bKe1IJv1iFjAYu0uA2G4fIN9cEagNRMQU4SDoIzm-y-dBWsgMFX4setmKtQbUNBAPy1Dg8Sj5Qq4ILdtpKKEkwXAw0ylJ6pPi9h3H-QN2B6zv5Og__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-                        class="h-16 cursor-pointer object-cover"
+                        alt="tribesquare-logo" 
                     />
-                </div>
-                
-            </div>
+                </li>
+
+            </ul>
         </div>
     </div>
 </template>
+
+<style scoped>
+
+.media-images {
+  margin: 2rem;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.media-images__image {
+  margin: 1rem;
+}
+
+.media-images.media-images--config-width .media-images__image {
+  width: 6.25rem;
+}
+
+.media-images.media-images--config-height .media-images__image {
+  height: 1.5rem;
+}
+</style>
