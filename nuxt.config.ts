48152bb6b557e6f136e80762e1cab6ccfb0c2db3
Anto Akoke Victor <antoakoke@gmail.com>
2023-06-07 00:13:37 +0100

    add meta information for index and contact pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://www.gravatar.com/avatar/a85051d369a9ee1131d28d56ec04fc17' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'https://www.gravatar.com/avatar/a85051d369a9ee1131d28d56ec04fc17' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: 'https://www.gravatar.com/avatar/a85051d369a9ee1131d28d56ec04fc17' }
+      ]
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
