785bfd8b7f7288aaecae8507858e32f526b7f2c8
Anto Akoke Victor <antoakoke@gmail.com>
2023-06-07 02:14:47 +0100

    use head hooks for meta

--- pages/index.vue
+++ pages/index.vue
@@ -7,23 +7,23 @@ export default defineComponent({
     components: {
         homePage
     },
-    head () {
-      return {
-        title: 'Software Developer based in Lagos, Nigeria - Akoke Victor Anto',
-        meta: [
-        { name: 'description', content: "Hi, my name is Akoke Victor Anto. I\'m a Software developer based in Lagos, Nigeria." },
-        { name: "twitter:card", content: "summary"},
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: "twitter:site", content: "@Iam_Veecktor"},
-        { name: "twitter:creator", content: "@Iam_Veecktor"},
-        { hid: 'og:title', name: "og:title", content: "Akoke Victor Anto - Software Developer based in Lagos, Nigeria"},
-        { hid: 'og-type', property: 'og:type', content: 'website' },
-        { hid: 'og:url', name: "og:url", content: "https://akokeanto.vercel.app/"},
-        { hid: 'og:image', name: "og:image", content: "https://www.gravatar.com/avatar/a85051d369a9ee1131d28d56ec04fc17"},
-        { hid: 'og:description', name: "og:description", content: "Hi, my name is Akoke Victor Anto. I\'m a Software developer based in Lagos, Nigeria."},
-        ]
-      }
-    }
+    setup() {
+        useHead({
+            title: 'Software Developer based in Lagos, Nigeria - Akoke Victor Anto',
+            meta: [
+                { name: 'description', content: "Hi, my name is Akoke Victor Anto. I\'m a Software developer based in Lagos, Nigeria." },
+                { name: "twitter:card", content: "summary"},
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { name: "twitter:site", content: "@Iam_Veecktor"},
+                { name: "twitter:creator", content: "@Iam_Veecktor"},
+                { hid: 'og:title', name: "og:title", content: "Akoke Victor Anto - Software Developer based in Lagos, Nigeria"},
+                { hid: 'og-type', property: 'og:type', content: 'website' },
+                { hid: 'og:url', name: "og:url", content: "https://akokeanto.vercel.app/"},
+                { hid: 'og:image', name: "og:image", content: "https://www.gravatar.com/avatar/a85051d369a9ee1131d28d56ec04fc17"},
+                { hid: 'og:description', name: "og:description", content: "Hi, my name is Akoke Victor Anto. I\'m a Software developer based in Lagos, Nigeria."},
+            ]
+        })
+    },
 });
 </script>
 
